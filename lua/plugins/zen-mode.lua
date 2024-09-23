46a7e307f408fd3857f19acca0bf19b46cd2fabe
Goodmorningpeople <zachy2020p@gmail.com>
2024-09-23 22:25:39 +0800

    fixed zen-mode config

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -13,22 +13,6 @@ return {
           showcmd = false, -- Disable the command in zen mode
         },
       },
-      on_open = function(win)
-        -- Save the current font size
-        vim.g.old_font_size = vim.opt.guifont:get()[1]
-        -- Set a large font size
-        vim.opt.guifont = "FiraCode Nerd Font:h24"
-        -- Adjust Alacritty font size
-        os.execute 'echo -e "\\e]710;FiraCode Nerd Font:h24\\a"'
-      end,
-      on_close = function()
-        -- Restore the original font size
-        vim.opt.guifont = vim.g.old_font_size
-        -- Restore Alacritty font size
-        os.execute 'echo -e "\\e]710;FiraCode Nerd Font:h12\\a"'
-      end,
     }
-    -- Set the key binding for toggling Zen mode
-    vim.api.nvim_set_keymap("n", "<leader>zz", ":ZenMode<CR>", { noremap = true, silent = true })
   end,
 }
